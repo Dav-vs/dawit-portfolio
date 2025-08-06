a6b60df4cadb206a4997648e9adb24d94043322b
junedi asli <junediasli901@gmail.com>
2025-08-06 15:43:17 +0300

    Fix server route and connect API to frontend

--- server/server.js
+++ server/server.js
@@ -95,3 +95,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 }); 
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to Dawit\'s Portfolio API!' });
+});
